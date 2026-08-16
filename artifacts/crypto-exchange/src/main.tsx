fcefeab90ce444775eb8aab883e81817c08bda13
Elsavierfinance2 <56832020-elsavierfinance@users.noreply.replit.com>
2026-08-16 15:17:31 +0000

    Fix blank page when site is translated (Google Translate DOM guard)

--- artifacts/crypto-exchange/src/main.tsx
+++ artifacts/crypto-exchange/src/main.tsx
@@ -2,4 +2,26 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Guard against Google Translate (and similar extensions) breaking React.
+// Translators replace DOM text nodes; React later tries to remove/insert
+// nodes it thinks it owns and throws NotFoundError, blanking the page.
+// Make those operations no-ops instead of fatal.
+if (typeof Node === "function" && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function <T extends Node>(this: Node, child: T): T {
+    if (child.parentNode !== this) {
+      return child;
+    }
+    return originalRemoveChild.call(this, child) as T;
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function <T extends Node>(this: Node, newNode: T, referenceNode: Node | null): T {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      return originalInsertBefore.call(this, newNode, null) as T;
+    }
+    return originalInsertBefore.call(this, newNode, referenceNode) as T;
+  };
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
